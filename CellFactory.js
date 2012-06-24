ead9e80aec978392fa189b634f4d776235029bb6
Jan Bölsche <regular.gonzales@googlemail.com>
2012-06-24 11:56:16 +0200

    editable labels, dropped images serve as icon or background, depending on size

--- CellFactory.js
+++ CellFactory.js
@@ -1,9 +1,59 @@
-function CellFactory() {
+function CellFactory(delegate) {
+    
+    function makeLabel(text) {
+        return $("<div>")
+            .html(text)
+            .addClass("label")
+            .click(function() {
+                var label = $(this);
+                var parent = $(this).parent();
+                $("<input>")
+                    .click(function() {return false;})
+                    .keypress(function(e) {
+                        if (e.keyCode == 13) {
+                            $(this).blur();
+                        }
+                    })
+                    .blur(function() {
+                        var newText = $(this).val();
+                        if (delegate) {
+                            delegate.labelTextChanged(newText)
+                        }
+                        $(this).remove();
+                        label.html(newText);
+                        label.show();
+                    })
+                    .insertAfter(label);
+                    
+                parent.find("input").focus();
+                label.hide();
+                return false;
+            });
+    }
+    
+    function handleImage(image, cell, dataUri) {
+        image.attr("src", dataUri);
+        
+        var w = image.width();
+        var h = image.height();
+        console.log("image dropped:", image.width(), "x", image.height())
+        
+        // if the image is large, we use it as a cell background
+        if (w>cell.width() || h>128) {
+            cell.css("background-image", "url(" + dataUri + ")");
+            image.hide();
+        } else {
+            image.show();
+        }
+    }
     
     return {
         makeCell: function(label, color) {
+            var image = $("<img>");
+            
             var cell = $("<td>")
-                .html(label)
+                .append(makeLabel(label))
+                .append(image)
                 .css("background-color", color)
                 
                 
@@ -25,6 +75,8 @@ function CellFactory() {
                 //evt.dataTransfer.dropEffect = 'link';
             });
             cell.bind("drop", function(evt) {
+                $(this).removeClass("dragTarget");
+                
                 var cell = $(this);
                 evt.stopPropagation();
                 evt.preventDefault();
@@ -35,8 +87,10 @@ function CellFactory() {
                     var reader = new FileReader();
                     reader.onloadend = function() {
                         var dataUri = reader.result;
-                        console.log(dataUri)
-                        cell.css("background-image", "url(" + dataUri + ")");
+                        if (delegate) {
+                            delegate.imageChanged(dataUri)
+                        }
+                        handleImage(image, cell, dataUri);
                     };
                     reader.readAsDataURL(file);
                 });
